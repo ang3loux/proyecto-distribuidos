b7c48e93090668f5b1f2f0a8247f7d46c5350e7f
Angelo Zambrano <angelo.zambrano.1990@gmail.com>
2018-06-09 17:03:48 -0400

    Changes

--- client.py
+++ client.py
@@ -2,6 +2,10 @@
 import socket
 import select
 import sys
+import os
+import random
+from time import sleep
+import json
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,22 +18,35 @@ port = int(sys.argv[2])
 
 server.connect((ipAddress, port))
 
+# def sleepThread():
+#     sleepTime = random.randint(1, 10)
+#     print sleepTime
+#     sleep(sleepTime)
+
+os.system('clear')
 while True:
+    jsonTest = {
+        'first_name': 'Guido',
+        'second_name': 'Rossum',
+        'titles': ['BDFL', 'Developer'],
+    }
 
     # maintains a list of possible input streams
     sockets_list = [sys.stdin, server]
-    
+
     read_sockets, write_socket, error_socket = select.select(
         sockets_list, [], [])
 
     for socks in read_sockets:
         if socks == server:
             message = socks.recv(2048)
-            print message
+            test = json.loads(message)
+            print test['first_name']
         else:
             message = sys.stdin.readline()
-            server.send(message)
-            sys.stdout.write("<You> ")
-            sys.stdout.write(message)
+            server.send(json.dumps(jsonTest))
+            sys.stdout.write("JSON enviado\n")
+            # sys.stdout.write(message)
             sys.stdout.flush()
+
 server.close()
--- server.py
+++ server.py
@@ -5,62 +5,66 @@ import sys
 import os
 from thread import *
 from time import sleep
+import json
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 if len(sys.argv) != 5:
-    print "Uso correcto: script, direccion IP, puerto, numero de clientes e iteraciones"
+    print "Uso correcto: script, direccion IP y puerto"
     exit()
 
 ipAddress = str(sys.argv[1])
 port = int(sys.argv[2])
-numberClients = int(sys.argv[3])
-iterations = int(sys.argv[4])
+numberClients = 3
 
 server.bind((ipAddress, port))
 server.listen(numberClients)
 
 connections = []
 
-
-def connectionThread(connection, address):
+def connection_thread(connection, address):
     connection.send("Conectado al servidor, esperando a los demas clientes...")
 
     while True:
         try:
             message = connection.recv(2048)
             if message:
-                print "<" + address[0] + "> " + message
+                # print "<" + address[0] + "> " + message
+                # connection.send("HOLA")
+                print message
+                connection.send(message)
 
             else:
-                removeConnection(connection)
+                remove_connection(connection)
 
         except:
             continue
 
 
-def broadcastMessage(message):
+def broadcast_message(message):
     for connection in connections:
         try:
             connection.send(message)
         except:
-            removeConnection(connection)
+            remove_connection(connection)
 
 
-def removeConnection(connection):
+def remove_connection(connection):
     if connection in connections:
         connection.close()
         connections.remove(connection)
         print "Cliente desconectado. Conexion cerrada."
 
 
-def codeMessage(message):
+def code_message(message):
     print "Code message"
 
 
-def decodeMessagge(message):
+def decode_messagge(message):
     print "Decode message"
+
+# def start_process():
 
 
 os.system('clear')
@@ -68,7 +72,6 @@ print "Iniciando servidor"
 print "IP: " + ipAddress
 print "Puerto: " + str(port)
 print "Nro de clientes: " + str(numberClients)
-print "Iteraciones: " + str(iterations)
 print "\nEsperando a clientes..."
 
 while numberClients > 0:
@@ -76,18 +79,13 @@ while numberClients > 0:
     connection, address = server.accept()
     connections.append(connection)
     print "<" + address[0] + "> conectado"
-    start_new_thread(connectionThread, (connection, address))
+    start_new_thread(connection_thread, (connection, address))
 
 sleep(1)
 print "\nClientes conectados, inicio de proceso..."
-broadcastMessage("Clientes conectados, inicio de proceso...")
-for connection in connections:
-    try:
-        connection.send("message")
-    except:
-        removeConnection(connection)
+broadcast_message("Clientes conectados, inicio de proceso...\n")
 
-dato = raw_input("\nPresione enter para terminar.")
+dato = raw_input("\nPresione enter para terminar.\n")
 print "\nCerrando conexiones..."
 for connection in connections:
     connection.close()
